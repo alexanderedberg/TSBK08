240739841b0529ac91bdbcd676b01dba335796ca
Alexander Edberg <alexander_edberg@hotmail.com>
2023-04-21 18:23:37 +0200

    works now, just need to loop through all files. Also when i decompress i get 1 byte smaller than original file.

--- huffman.py
+++ huffman.py
@@ -1,16 +1,5 @@
-#import numpy as np
 from collections import Counter
 
-
-
-file = "cantrbry/asyoulik.txt" #asyoulik.txt #cantrbry #bible.txt #E.coli #alice29.txt
-
-"""
-with open(file,"rb") as f:
-
-        file_data = f.read()
-        print(file_data)
-"""
 
 def joint_entropy(mem,file):
 
@@ -66,10 +55,10 @@ class Nodes:
             self.right.inorderTraversal(res)
             res = res[:-1]
 
-    def decode_huffman(self, root, file):
-
-        with open(file,"r") as f , open("beepboop","wb") as o:
-            
+    def decode_huffman(self, root, bin):
+        #file = file[:-5]
+        with open(bin,"r") as f , open(file + "_decompressed","wb") as o:
+            #f = f[:-5]
             while True:
                 char = f.read(1)
                 #print(char)
@@ -90,7 +79,7 @@ class Nodes:
                     self = self.right
 
 
-
+file = "cantrbry/alice29.txt" #asyoulik.txt #cantrbry #bible.txt #E.coli #alice29.txt
 
 proba_dict = joint_entropy(0,file) #"cantrbry/alice29.txt"
 
@@ -125,20 +114,25 @@ node.inorderTraversal(res) #This is the last node created, so the root node with
 #encode
 
 #read each symbol and write it as its' codeword
-with open(file,"rb") as f , open("demofile2","w") as o:
+with open(file,"rb") as f , open("temp","w") as o:
 
     while True:
         char = f.read(1)
+        #print(char)
         if not char:
             #print("End of file")
+            #print(char)
             break
         
         o.write(codeword_dict[char])
 
+#print(char)
+#print(codeword_dict[])
+
 
 #pad the file with 0 so that the number of bits are divisible by 8, after that read the file with 8bits at a time, convert the 8bit representation to an int and then
 #convert that to byte and write.
-with open("demofile2","r+") as o, open("encodetest","wb") as output: 
+with open("temp","r+") as o, open(file + "_compressed","wb") as output: 
 
     bitstream = o.read()
     bitstream_len = len(bitstream)
@@ -175,22 +169,30 @@ with open("demofile2","r+") as o, open("encodetest","wb") as output:
 
 
 #decode
-file = "encodetest"
-with open(file,"rb") as f , open("decodetest","w") as o:
+#file = "encodetest"
+with open(file + "_compressed","rb") as f , open("binary","w") as o:
 
     #read each byte, convert to binary and write to new file
+    string = ""
     while True:
         char = f.read(1)
         if not char:
             break
         
-        o.write('{0:08b}'.format(int.from_bytes(char, "big")))
+        string += '{0:08b}'.format(int.from_bytes(char, "big"))
+        #o.write('{0:08b}'.format(int.from_bytes(char, "big")))
+    #print(string)
+    #string = string[:-5]
+    o.write(string)
+    #o.write('{0:08b}'.format(int.from_bytes(char, "big")))
 
-    
-node.decode_huffman(node,"decodetest")
 
 
-print('{0:08b}'.format(int.from_bytes(char, "big"))) #do this in loop and write to a file, then remove the padding (how do we know how much padding there is?), 
+
+node.decode_huffman(node,"binary")
+
+
+#print('{0:08b}'.format(int.from_bytes(char, "big"))) #do this in loop and write to a file, then remove the padding (how do we know how much padding there is?), 
                                                      #then read and use the dict in reverse??
 
 
