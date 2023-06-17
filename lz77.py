3ecf6a21481469f93f4998a0bd1d8beed032e143
Alexander Edberg <alexander_edberg@hotmail.com>
2023-06-17 21:38:53 +0200

    very slow right now, i think it doesnt get good matchlength

--- lz77.py
+++ lz77.py
@@ -34,41 +34,70 @@ with open(file,"rb") as f:
 codeword_array = []
 search_index = 0
 i = 0
-while i < 5: #len(data_array):
-    print("hello this is run nummber : " + str(i))
+print(len(data_array))
+while i < 100: #len(data_array): #len(data_array):
+    #print("hello this is run nummber : " + str(i))
     #look at symol on pos i, match it to symol to the left (so i - x), where x is capped at the size of the search buffer. (i+y) y = look-ahead size is the cap for lookahead buffer
     #data_array[i]
 
 
     #find  one symbol match
+    match = ()
+    best_match = ()
+    l = 0
     for j in range(search_index,0,-1):
-        print(j)
+        #print(j)
 
         if data_array[i] == data_array[i-j]: #if match with ONE symbol is found
 
             l = 1
-            match = data_array[i]
+            best_match = (abs(i-j), l, data_array[i+1])
+            #print("-------------------------------------------")
+            #print(data_array[i])
+            #print(data_array[i+l])
+            #print(data_array[(i-j)+l])
 
-            while data_array[i:i+l] == data_array[i:(i-j)+l]: #see how many symbols are matching after the first found symbol match
+            while data_array[i+l] == data_array[(i-j)+l]: #see how many symbols are matching after the first found symbol match
                 
-                match = data_array[i:i+l]
-                l +=1
+                #match = data_array[i:i+l]
+                #print("hej")
+                l += 1
 
-            (o,l,data_array[i+l+1]) #är o abs av i-j ???
+            match = (abs(i-j), l, data_array[i+l+1]) # är o = abs(i-j)???? Kan behöva ta -1 till för att indexera på 0. (o,l,symbol), l = l-1 pga loopen
+            #print(l)
+            #print("-----------------------------")
+            #print(match[1])
+            #print(best_match[1])
+            if match[1] > best_match[1]: # index 1 to compare the match length
+                #print(match[1])
+                #print(best_match[1])
+                best_match = match
+                #(o,l,data_array[i+l+1]) #är o abs av i-j ???
 
+        if j == 1 and best_match == (): # if this is true then no matching symbol was found
 
-        #UPDATE SEARCH INDEX WHERE??? searcg_index = l+1
+            codeword_array.append((0,l,data_array[i]))
+            #print("den e tom")
+            #print((0,l,data_array[i]))
+        elif j == 1:
+            codeword_array.append(best_match)
+            #print("vi hittade match")
+            #print(best_match)
+
+        #UPDATE SEARCH INDEX WHERE??? searcg_index = l, ty allt kommer med i loopen
 
 
         #data_array[]
 
-    search_index += 1
+    search_index += l+1
+    #print(search_index)
+
     #if #matched symbols is 0, i += 1
 
     #else # i = matched symbols (l) + 1
-    i += 1
+    i += l+1
 
-
+print(codeword_array)
 #fill look-ahead buffer
 
 #for i in range (0,look_ahead_buffer_size):
